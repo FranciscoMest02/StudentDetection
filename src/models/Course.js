2006f21ba13023bb2cd640053848d4432f00f048
FranciscoMest02 <A01731549@tec.mx>
2023-10-28 15:28:40 -0600

    Pagina para ver curso

---
+++ src/models/Course.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const courseSchema = new Schema({
+    /*_id: {
+      $oid: String,
+    },*/
+    name: String,
+    teacher: String,
+    teacherId: String,
+    students: [
+      {
+        name: String,
+        studentId: String,
+      },
+    ],
+    days: [String],
+    time: {
+      start: String,
+      end: String,
+    },
+    attendance: [
+      {
+        date: Date,
+        name: String,
+        studentId: String,
+      },
+    ],
+  }, {
+    collection: 'courses'
+  });
+
+export default models.Course || model('Course', courseSchema)
