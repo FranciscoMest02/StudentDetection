1ba97dc1e193c341333c65e363b457298cceaf5a
FranciscoMest02 <A01731549@tec.mx>
2023-10-28 15:29:11 -0600

    Changes to sidebar

--- src/app/componentes/Sidebar.jsx
+++ src/app/componentes/Sidebar.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
+    <div className="w-1/4">
       {/* Contenido del Sidebar */}
+      <p>Prueba del </p>
+      <p>scsdcsdcs</p>
+      <p>scsdcsdcs</p>
+      <p>scsdcsdcs</p>
     </div>
   );
 }
