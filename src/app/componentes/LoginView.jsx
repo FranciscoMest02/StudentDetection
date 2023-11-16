85f6cac6d2a28ca7270dc42eb386773360098a89
FranciscoMest02 <A01731549@tec.mx>
2023-11-16 14:40:57 -0600

    Cambio de texto y color al boton de login

--- src/app/componentes/LoginView.jsx
+++ src/app/componentes/LoginView.jsx
@@ -37,8 +37,8 @@ const LoginView = () => {
 
             <form onSubmit={onSubmit}>
                 <div className='mt-8'>
-                    <label htmlFor="mail" className='block font-bold pb-2'>Correo electrónico</label>
-                    <input id="mail" type="text" name="mail" placeholder='ejemplo@gmail.com' className='w-96 border border-gray-500 rounded-md px-2 py-2' disabled={loading}/>
+                    <label htmlFor="mail" className='block font-bold pb-2'>Nombre de usuario</label>
+                    <input id="mail" type="text" name="mail" placeholder='Escribe tu nombre de usuario' className='w-96 border border-gray-500 rounded-md px-2 py-2' disabled={loading}/>
                 </div>
                 
                 <div className='mt-8'>
@@ -52,10 +52,10 @@ const LoginView = () => {
                         <span className='ml-2 text-xs'>Mantener mi sesión iniciada</span>
                     </label>
                     
-                    <span className='place-self-end'><a href='#' className='ml-2 text-xs underline font-semibold'>Olvidé mi contraseña</a></span>
+                    <span className='place-self-end'><a className='ml-2 text-xs underline font-semibold'>Olvidé mi contraseña</a></span>
                 </div>
                 <div className='flex justify-center mt-16 '>
-                    <button type="submit" className='border px-16 py-2 rounded-xl bg-blue-700 text-white' disabled={loading}>{!loading ? 'Iniciar sesión' : 'Verificando...'}</button>
+                    <button type="submit" className={'border px-16 py-2 rounded-xl bg-blue-700 text-white ' + (!loading ? 'bg-blue-600' : 'bg-blue-900')} disabled={loading}>{!loading ? 'Iniciar sesión' : 'Verificando...'}</button>
                 </div>
             </form>
 
